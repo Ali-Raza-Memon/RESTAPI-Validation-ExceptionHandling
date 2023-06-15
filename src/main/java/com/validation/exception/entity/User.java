69812dc29d6d10338252171f552f9c2206f9761a
Ali-Raza-Memon <alirazamemon0570@gmail.com>
2023-06-15 15:36:31 +0500

    Added User class with its attributes

---
+++ src/main/java/com/validation/exception/entity/User.java
@@ -0,0 +1,24 @@
+package com.validation.exception.entity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name="User")
+public class User {
+    @Id
+    @GeneratedValue
+    private int userId;
+    private String name;
+    private String email;
+    private String mobile;
+    private String gender;
+    private int age;
+    private String nationality;
+}
