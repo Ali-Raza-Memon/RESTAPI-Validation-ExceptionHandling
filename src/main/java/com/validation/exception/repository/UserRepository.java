a40e2df9a8aa5639ae5eb42bd3cf94cd1129ce78
Ali-Raza-Memon <alirazamemon0570@gmail.com>
2023-06-15 16:24:26 +0500

    Methods are created in service layer

--- src/main/java/com/validation/exception/repository/UserRepository.java
+++ src/main/java/com/validation/exception/repository/UserRepository.java
@@ -4,4 +4,5 @@ import com.validation.exception.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User,Integer> {
+    User findByUserId(int id);
 }
--- src/main/java/com/validation/exception/service/UserService.java
+++ src/main/java/com/validation/exception/service/UserService.java
@@ -5,6 +5,10 @@ import com.validation.exception.entity.User;
 import com.validation.exception.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
 
 @Service
 public class UserService  {
@@ -13,11 +17,29 @@ public class UserService  {
     private UserRepository userRepository;
 
 
-    public User saveUser(UserRequest userRequest){
-        User user = User.build(0,userRequest.getName(),
-                userRequest.getAge(),
-                userRequest.getGender(),userRequest.getMobile(),
-                userRequest.getNationality());
+//    public User saveUser(UserRequest userRequest){
+//        User user = User.build(0,userRequest.getName(),
+//                userRequest.getAge(),
+//                userRequest.getGender(),userRequest.getMobile(),
+//                userRequest.getNationality());
+//
+//        return userRepository.save(userRequest);
+//    }
+
+
+
+
+    public User saveUser(@RequestBody User user){
+        return userRepository.save(user);
+    }
+
+
+    public List<User> getUser(){
+        return userRepository.findAll();
+    }
+
+    public User findById(int id){
+        return userRepository.findByUserId(id);
     }
 
 
